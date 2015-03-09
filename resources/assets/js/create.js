7baadc607bb88afcdb75cae067882ef7928aac92
Aisling Brock <aisling@message.co.uk>
2015-03-09 11:48:05 +0000

    add disable button into callback

--- resources/assets/js/create.js
+++ resources/assets/js/create.js
@@ -1,7 +1,8 @@
 $(function () {
     $('[data-load-form]').on('click', function(e) {
         e.preventDefault();
-        $('#save-button').attr('disabled', false);
-        $('#options-form').load($(this).attr('href'));
+        $('#options-form').load($(this).attr('href'), function() {
+	        $('#save-button').attr('disabled', false);
+        });
     });
 });

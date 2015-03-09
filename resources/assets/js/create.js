4d9f9093c5009b15bb8d813ac6738fc102810196
Aisling Brock <aisling@message.co.uk>
2015-03-09 11:46:36 +0000

    changing button positions

--- resources/assets/js/create.js
+++ resources/assets/js/create.js
@@ -1,6 +1,7 @@
 $(function () {
     $('[data-load-form]').on('click', function(e) {
         e.preventDefault();
+        $('#save-button').attr('disabled', false);
         $('#options-form').load($(this).attr('href'));
     });
 });

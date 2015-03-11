76b4947763fadeabe4f6fe0af77958ad54671039
thomasjthomasj <thomas@Thomass-MacBook-Pro.local>
2015-03-11 11:10:02 +0000

    radio buttons on reward type select form

--- resources/assets/js/create.js
+++ resources/assets/js/create.js
@@ -1,4 +1,7 @@
 $(function () {
+    $('.select-reward-type').on('click', function(e) {
+        e.preventDefault();
+    });
     $('[data-load-form]').on('click', function(e) {
         e.preventDefault();
         $('#options-form').load($(this).attr('href'), function() {
@@ -7,6 +10,6 @@ $(function () {
         $('.select-reward-type').prop('checked', false);
 
         var typeName = $(this).attr('href').split('/').slice(-1).pop();
-        $('#radio-' + typeName).prop('checked', true);
+        $('#radio_' + typeName).prop('checked', true);
     });
 });

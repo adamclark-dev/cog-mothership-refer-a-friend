7de304099a5ab1a7c963df90932e42866355bd49
thomasjthomasj <thomas@Thomass-MacBook-Pro.local>
2015-03-11 10:52:25 +0000

    radio buttons on type select

--- resources/assets/js/create.js
+++ resources/assets/js/create.js
@@ -4,5 +4,9 @@ $(function () {
         $('#options-form').load($(this).attr('href'), function() {
 	        $('#save-button').attr('disabled', false);
         });
+        $('.select-reward-type').prop('checked', false);
+
+        var typeName = $(this).attr('href').split('/').slice(-1).pop();
+        $('#radio-' + typeName).prop('checked', true);
     });
 });
